ead4e8c6b4e0c73a2c94e94c00a98d2b8bf2f38e
MoScript <littletoadfan@mailinator.com>
2016-08-23 11:44:14 +0800

    Create highlight.js

---
+++ mogic-highlighter/highlight.js
@@ -0,0 +1,37 @@
+(function() {
+  function loadScript(url, callback) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    if (script.readyState) { //IE
+      script.onreadystatechange = function() {
+        if (script.readyState == "loaded" || script.readyState == "complete") {
+          script.onreadystatechange = null;
+          callback();
+        }
+      };
+    } else {
+      script.onload = function() {
+        callback();
+      };
+    }
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+  var host = "//moscript.github.io/mogic-highlighter/";
+  loadScript(host + "mark.min.js", (function() {
+    loadScript(host + "mogic-dict.js", (function() {
+      var instance = new Mark(document.getElementsByTagName("body")[0]);
+      instance.mark(mogic);
+    }));
+  }));
+  /*
+if (typeof Mark == 'undefined') {
+    var init = false;
+    loadScript(host + "mark.min.js", (function() {
+      highlight();
+    }));
+  } else {
+    highlight();
+  }
+*/
+})();
