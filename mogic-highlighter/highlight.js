ed51a1fd28c1cde866beb31eb0ced011618b1241
MoScript <littletoadfan@mailinator.com>
2016-08-23 11:58:23 +0800

    Update highlight.js

--- mogic-highlighter/highlight.js
+++ mogic-highlighter/highlight.js
@@ -17,6 +17,11 @@
     script.src = url;
     document.getElementsByTagName("head")[0].appendChild(script);
   }
+  
+  var sheet = document.createElement('style');
+  sheet.innerHTML = "mark{background:rgba(136,245,39,0.42);box-shadow:0px 0px 5px #88f527}";
+  document.body.appendChild(sheet);
+  
   var host = "//moscript.github.io/mogic-highlighter/";
   loadScript(host + "mark.min.js", (function() {
     loadScript(host + "mogic-dict.js", (function() {
