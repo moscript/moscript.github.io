148fb08153f790d0eca65d2ad44786521cbf6ba5
MoScript <littletoadfan@mailinator.com>
2016-10-25 10:39:34 +0800

    +1s

---
+++ click-n-extend/click-n-extend.js
@@ -0,0 +1,53 @@
+var n = 0;
+
+var loadCSS = document.createElement("link");
+setAttributes(loadCSS, {
+  "rel": "stylesheet",
+  "type": "text/css",
+  "href": "click-n-extend.css"
+})
+document.getElementsByTagName("head")[0].appendChild(loadCSS);
+
+function setAttributes(el, attrs) {
+  for (var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+
+function extend(event) {
+  n++;
+  var onesec = document.createElement("img");
+  setAttributes(onesec, {
+    "id": "sec" + n,
+    "class": "seconds",
+    "src": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAALCAMAAACj4OZyAAAAXVBMVEUAAAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAD/AAAHHvfOAAAAHnRSTlMA9uD83ZruPemHWjQX1NLCsoJoYCUNBPfFrKd2HxFL21rZAAAAa0lEQVQY023PiQqAIAwGYHVmdp/avfd/zMzAIxsI7mOMf8RVz3nnu8BHyiKoZPF+Pp5jFrpqhRBT6mN7XjsjAIAIUDo/Mm2H6IJAqfea4Vr97Cd6klCkXivzxJOTRTmbbZ4HGQZu7L0DN1Xe5zYEOudtMvoAAAAASUVORK5CYII=",
+    "style": "left:" + event.pageX + "px" + ";top:" + event.pageY + "px" + ";"
+  });
+  document.body.appendChild(onesec);
+}
+
+function display(e) {
+  var n = document.getElementsByClassName("seconds");
+  switch (e.keyCode) {
+    case 69:
+      for (i = 0; i < n.length; i++) {
+        n[i].style.opacity = 1;
+      };
+      break;
+    case 53:
+      for (i = 0; i < n.length; i++) {
+        n[i].style.opacity = 0;
+      };
+      break;
+    case 84:
+      for (i = 0; i < n.length; i++) {
+        n[i].style.opacity = 1;
+      };
+      alert("You had prolonged " + n.length + "s for the Elder.");
+      break;
+  }
+  return n.length;
+}
+
+document.addEventListener("keydown", display, false);
+document.addEventListener("mousedown", extend);
