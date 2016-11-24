0477d2fd83f7783d611bd0db1b86e4e61a05b3fb
MoScript <littletoadfan@mailinator.com>
2016-11-24 12:46:06 +0800

    Update click-n-extend.js

--- click-n-extend/click-n-extend.js
+++ click-n-extend/click-n-extend.js
@@ -32,16 +32,12 @@ var 续命 = {
       for (i = 0; i < seconds.length; i++) {
         seconds[i].style.opacity = 1;
       };
-      if (seconds.length > 0) {
-        console.info("%c你为长者续了%c" + seconds.length + "s", "color:#9e9e9e", "color:#f44336");
-      };
-      return seconds.length;
+      return console.info("%c你为长者续了%c" + seconds.length + "s", "color:#9e9e9e", "color:#f44336");
   }) (),
   "闷声": (function () {
       for (i = 0; i < seconds.length; i++) {
         seconds[i].style.opacity = 0;
       };
-      return seconds.length;
   }) ()
 }
 console.info("%c温馨提示：输入%c 续命.知多少 %c来显示在当前页面中所续的秒数，输入%c 续命.闷声 %c来隐藏所续秒数。", "color:#9e9e9e", "color:#2196f3", "color:#9e9e9e", "color:#2196f3", "color:#9e9e9e");
